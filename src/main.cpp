143b9efd295cfad45c01c25d4f7cc1d4386167e4
Jake <jakepyne@gmail.com>
2013-02-26 10:21:57 +0100

    Parse CLI args

--- src/main.cpp
+++ src/main.cpp
@@ -7,35 +7,66 @@
 //
 
 #include <iostream>
+#include <string>
+#include <vector>
 #include "fmod.hpp"
 #include "fmod_errors.h"
+
+#define MAX_AUDIO_FILES 32
+#define VERSION 0.01
+#define USAGE "Usage: resound \"/path/to/audiofile\" \"/path/to/audiofile_two\" --config \"/path/to/config_file\""
 
 void checkErr(FMOD_RESULT res)
 {
     if (res!= FMOD_OK)
     {
-        std::cerr << "ERR: " << FMOD_ErrorString(res);
+        std::cerr << "FMOD ERR: " << FMOD_ErrorString(res);
         exit(-1);
     }
 }
 
 int main(int argc, const char * argv[])
 {
-
-    /*
-     * TODO error handling for cli args
-     */
+    std::string path_config;
+    std::vector<std::string> path_audio;
+    
+    if (argc < 2) {
+        std::cout << USAGE << std::endl;
+        exit(-1);
+    }
+    
+    if (!strncmp(argv[1], "-v", 2)) {
+        std::cout << "Version: " << VERSION << std::endl;
+        exit(0);
+    }
+    else if (!strncmp(argv[1], "--help", 6)) {
+        std::cout << USAGE << std::endl;
+        exit(0);
+    }
+    
+    bool cfg_set = false;
+    for (int i = 1; i < argc; i++) {
+        
+        if (!cfg_set) {
+            if (!strncmp(argv[i], "--config", 8) || !strncmp(argv[i], "-c", 2)) {
+                cfg_set = true;
+            }
+            else {
+                path_audio.push_back(argv[i]);
+            }
+        }
+        else {
+            path_config = argv[i];
+            cfg_set = false;
+        }
+    }
     
     FMOD::System     *system;
     FMOD::Sound      *sound;
     FMOD::Channel    *channel = 0;
     FMOD_RESULT       result;
-    unsigned int      version;
     
     result = FMOD::System_Create(&system);
-    checkErr(result);
-    
-    result = system->getVersion(&version);
     checkErr(result);
     
     result = system->init(32, FMOD_INIT_NORMAL, NULL);
