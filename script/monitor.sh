00ac8bf649019315dcb6227aefa4240463c72adc
Jake <jakepyne@gmail.com>
2013-02-27 22:30:25 +0100

    Update name in shell script

--- script/monitor.sh
+++ script/monitor.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# This script will keep the audio player running no matter what
+# This script will keep the audio player running
 # Make sure that the player binary is in your PATH and that the config file define is set below
 
-CONFIG_FILE="config.yml"
+CONFIG_FILE="../example/config.yml"
 
-until playq --config "$CONFIG_FILE"; do
-  echo "Audio player crashed, exit code $?." >&2
+until resound --config "$CONFIG_FILE"; do
+  echo "Resound crashed, exit code $?." >&2
   sleep 1
 done
